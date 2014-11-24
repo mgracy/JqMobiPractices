85206612d4e2138a1453062099c0b125ed2872b6
Gracy.Ma <glmgracy@gmail.com>
2014-11-24 21:26:29 +0800

    Add Login as well as index pages

---
+++ nanyida/nanyida/ViewController.swift
@@ -0,0 +1,33 @@
+//
+//  ViewController.swift
+//  nanyida
+//
+//  Created by  马桂新 on 14/11/24.
+//  Copyright (c) 2014年 mgracy.blog.163.com. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet var stuNo: UITextField!
+    
+    @IBOutlet var stuPwd: UITextField!
+    
+    @IBAction func btnLogin(sender: UIButton) {
+        
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ nanyida/nanyida/xnggViewController.swift
@@ -0,0 +1,9 @@
+//
+//  xnggViewController.swift
+//  nanyida
+//
+//  Created by  马桂新 on 14/11/24.
+//  Copyright (c) 2014年 mgracy.blog.163.com. All rights reserved.
+//
+
+import Foundation
